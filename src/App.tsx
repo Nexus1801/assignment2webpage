fb81a5e8860888ccd646496988c2ac3cd9aeaeb7
Nexus1801 <ubervenx@gmail.com>
2024-03-24 21:48:10 -0400

    revert html

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './list.css'
 import './title.tsx'
+import title from "./title.tsx";
 import './intro.tsx'
 import './list.tsx'
 import './list.css'
@@ -24,12 +25,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+        <title>name</title>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
+
         </p>
       </div>
       <p className="read-the-docs">
---
+++ src/title.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function title(props){
+    const name  = 'Tri Vien Le and Alexander Lap'
+
+    return <h1>{name}</h1>
+}
+
+export default title;
